cc8d679074bb697d1f79fe4e8b626d12102b941a
Håvard O. Nordstrand <haavard@haavard.tech>
2019-11-26 23:40:57 +0100

    Invert distortion setting

--- src/effects.c
+++ src/effects.c
@@ -69,7 +69,7 @@ void setup_effects() {
 
 	effect_t bitcrush = create_effect("Bitcrush & Dist.", 4);
 	bitcrush.settings[0] = create_setting(0, "Bypass", "", 1, 1, 0, 1);
-	bitcrush.settings[1] = create_setting(13, "Amplitude", "%", 100, 10, 0, 100);
+	bitcrush.settings[1] = create_setting(13, "Amplitude", "%", 0, 10, 0, 100);
 	bitcrush.settings[1]->map_setting_value = map_div10;
 	bitcrush.settings[2] = create_setting(1, "Bits", "bits", 0, 1, 0, 15);
 	bitcrush.settings[3] = create_setting(2, "Rate", "", 1, 1, 1, 60);
