15b9789b5da9ed65c727b97e3a6f6afe362f78b9
Håvard O. Nordstrand <haavard@haavard.tech>
2019-12-05 22:31:06 +0100

    Change distortion setting to use bits

--- src/effects.c
+++ src/effects.c
@@ -70,10 +70,9 @@ void setup_effects() {
 
 	effect_t bitcrush = create_effect("Bitcrush & Dist.", 4);
 	bitcrush.settings[0] = create_setting(0, "Bypass", "", 1, 1, 0, 1);
-	bitcrush.settings[1] = create_setting(13, "Distortion", "%", 0, 10, 0, 100);
-	bitcrush.settings[1]->map_setting_value = map_div10;
-	bitcrush.settings[2] = create_setting(1, "Bits", "bits", 0, 1, 0, 15);
-	bitcrush.settings[3] = create_setting(2, "Rate", "", 1, 2, 1, 61);
+	bitcrush.settings[1] = create_setting(13, "Distortion", "bits", 0, 1, 0, 15);
+	bitcrush.settings[2] = create_setting(1, "Depth", "bits", 0, 1, 0, 15);
+	bitcrush.settings[3] = create_setting(2, "Rate", "div", 1, 2, 1, 61);
 
 	effects[0] = bitcrush;
 	effects[1] = tremolo;
